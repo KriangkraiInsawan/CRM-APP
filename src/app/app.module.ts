a778da5d32014ed0fe2172536befb0cf3b8782d0
kriangkrai <kriangkrai86@gmail.com>
2021-01-19 15:21:07 +0700

    gen-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Layout1Component } from './layout/layout1/layout1.component';
+import { ProgressbarComponent } from './layout/progressbar/progressbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Layout1Component,
+    ProgressbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
