36fedcbb54617c3d20cb2dde3263b4ad6c1fd99c
kriangkrai <kriangkrai86@gmail.com>
2021-01-20 11:26:20 +0700

    gen login/resgist/reset

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent, ResetpasswordComponent],
   imports: [
     CommonModule
   ]
