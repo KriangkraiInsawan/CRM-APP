f1ffb7b34b85f1601732c29ff2b4777718b06bf1
kriangkrai <kriangkrai86@gmail.com>
2021-01-25 12:54:22 +0700

    gencustomer

--- src/app/modules/left-form/left-form.module.ts
+++ src/app/modules/left-form/left-form.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
 
 
 @NgModule({
-  declarations: [ContactFormComponent],
+  declarations: [ContactFormComponent, CustomerFormComponent],
   imports: [
     CommonModule,
     SharedModule
