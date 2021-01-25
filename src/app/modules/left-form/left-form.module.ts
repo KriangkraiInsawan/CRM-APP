89b34801d279085b05eb861c38ab10caef280548
kriangkrai <kriangkrai86@gmail.com>
2021-01-25 12:55:36 +0700

    gen deal

--- src/app/modules/left-form/left-form.module.ts
+++ src/app/modules/left-form/left-form.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
+import { DealFormComponent } from './deal-form/deal-form.component';
 
 
 
 @NgModule({
-  declarations: [ContactFormComponent, CustomerFormComponent],
+  declarations: [ContactFormComponent, CustomerFormComponent, DealFormComponent],
   imports: [
     CommonModule,
     SharedModule
