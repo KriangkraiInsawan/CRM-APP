436d72a908204977f7a27ff71f52831de371df0f
kriangkrai <kriangkrai86@gmail.com>
2021-01-25 12:52:31 +0700

    gen contact

--- src/app/modules/left-form/left-form.module.ts
+++ src/app/modules/left-form/left-form.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ContactFormComponent],
   imports: [
     CommonModule,
     SharedModule
