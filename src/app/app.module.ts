68ca2fa0a44a87cde8a249fde1b40d1cff613b50
kriangkrai <kriangkrai86@gmail.com>
2021-01-25 12:01:05 +0700

    gen interceptor/error.handle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Layout1Module } from './layout/layout1/layout1.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppInterceptor } from './app.interceptor';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -14,10 +17,21 @@ import { Layout1Module } from './layout/layout1/layout1.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
+
     Layout1Module
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS,useClass:AppInterceptor,multi:true},
+    {provide:MAT_DATE_LOCALE,useValue:'en-GB'}
+     /**
+     * เปิด ErrorHandler เมื่อ Deploy app ขึ้น Prroduction environment
+     * ปิด ErrorHandler เมืออยู่ระหว่าง Development เพื่อ debug error
+     */
+    // { provide: ErrorHandler, useClass: AppErrorHandler },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
