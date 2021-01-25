5e856751e252fb0176399acdac33cdfe81aaed1d
kriangkrai <kriangkrai86@gmail.com>
2021-01-25 13:02:24 +0700

    leftform

--- src/app/modules/left-form/left-form.module.ts
+++ src/app/modules/left-form/left-form.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
 import { DealFormComponent } from './deal-form/deal-form.component';
+import { LeftFormComponent } from './left-form.component';
 
 
 
@@ -12,6 +13,7 @@ import { DealFormComponent } from './deal-form/deal-form.component';
   imports: [
     CommonModule,
     SharedModule
-  ]
+  ],
+  exports:[LeftFormComponent]
 })
 export class LeftFormModule { }
