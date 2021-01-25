201535a25fa3d0f3b033b498d2ec10b954149979
kriangkrai <kriangkrai86@gmail.com>
2021-01-25 13:07:45 +0700

    gen c /m

---
+++ src/app/modules/generic-master/generic-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-master',
+  templateUrl: './generic-master.component.html',
+  styleUrls: ['./generic-master.component.scss']
+})
+export class GenericMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
